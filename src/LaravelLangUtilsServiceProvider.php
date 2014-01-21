d053120bbbf6ede2020a1d29392b0a097d609154
Stef Horner <shorner@wearearchitect.com>
2014-01-21 12:01:18 +0000

    rename service provider

--- src/LaravelLangUtilsServiceProvider.php
+++ src/LaravelLangUtilsServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelLangsServiceProvider extends ServiceProvider {
+class LaravelLangUtilsServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
