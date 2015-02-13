90841435c35259f88ec10c51cc401f7ba7480a20
Stef Horner <stef@tedslittlerobot.com>
2015-02-13 17:01:07 +0000

    add test for import command

---
+++ tests/ImportCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class ImportCommandTest extends PHPUnit_Framework_TestCase {
+
+	public function setup()
+	{
+		$this->cmd = new Tlr\LaravelLangTools\ImportCommand;
+	}
+
+	public function testArrayHydrateArray()
+	{
+		$input = [ 'one' => 'two' ];
+		$result = $this->cmd->hydrateArray( $input );
+
+		$this->assertEquals($input, $result);
+
+		$input = [ 'one.two.three' => 'woop' ];
+		$value = ['one' => ['two' => ['three' => 'woop']]];
+
+		$result = $this->cmd->hydrateArray( $input );
+
+		$this->assertEquals( $value, $result );
+
+		$input = [ 'one.two.three' => 'woop', 'one.four' => 'foo' ];
+		$value = ['one' => ['two' => ['three' => 'woop'], 'four' => 'foo']];
+
+		$result = $this->cmd->hydrateArray( $input );
+
+		$this->assertEquals( $value, $result );
+	}
+
+
+	public function testArrayHydrateArrays()
+	{
+		$input = [
+			'one' => ['test' => 'foo'],
+			'two' => ['test' => 'bar'],
+		];
+		$result = $this->cmd->hydrateArrays( $input );
+
+		$this->assertEquals($input, $result);
+	}
+
+}
